7aed04f8d64930f0dd7ef1246982e24e08cf6090
yoga-tirta <yogatirtapermana552@gmail.com>
2022-03-10 15:34:22 +0700

    merubah file variable.php

--- tugas2/variable.php
+++ tugas2/variable.php
@@ -3,4 +3,13 @@
 $a = 7;
 $b = 3;
 
+$penjumlahan = $a + $b;
+echo $a." + ".$b." = ".$penjumlahan."<br>";
+
+$pengurangan = $a - $b;
+echo $a." - ".$b." = ".$pengurangan."<br>";
+
+$perkalian = $a * $b;
+echo $a." * ".$b." = ".$perkalian;
+
 ?>
