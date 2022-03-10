24ce9dbf815913c2154122b54dc61c3905ea99cf
yoga-tirta <yogatirtapermana552@gmail.com>
2022-03-10 15:43:58 +0700

    merubah file fungsi.php

---
+++ tugas2/fungsi.php
@@ -0,0 +1,11 @@
+<?php 
+
+function hello($nama) {
+    echo "<p>Selamat datang, {$nama} !</p>";
+}
+
+hello("Yoga");
+hello("Tirta");
+hello("Permana");
+
+?>
--- tugas2/variable.php
+++ tugas2/variable.php
@@ -4,12 +4,11 @@ $a = 7;
 $b = 3;
 
 $penjumlahan = $a + $b;
-echo $a." + ".$b." = ".$penjumlahan."<br>";
-
 $pengurangan = $a - $b;
-echo $a." - ".$b." = ".$pengurangan."<br>";
-
 $perkalian = $a * $b;
+
+echo $a." + ".$b." = ".$penjumlahan."<br>";
+echo $a." - ".$b." = ".$pengurangan."<br>";
 echo $a." * ".$b." = ".$perkalian;
 
 ?>
